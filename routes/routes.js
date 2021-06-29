ee54993ff67f98a88ad39c985cc521ee36dc7788
Shivendra Singh Thakur <Shivendra Singh Thakur>
2021-06-29 18:55:43 +0530

    boot

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,15 @@ routes.get('/login', (req, res)=>{
     res.render('login',{})
 });
 
+routes.get('/room', (req, res)=>{
+    res.render('login',{})
+});
+
+routes.post('/room', (req, res)=>{
+    var data = req.body;
+	console.log(data.roomId);
+    res.render('room',{data : data})
+
+});
+
 module.exports = routes;
